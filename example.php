f0b5c324b15989101fec4d8464036bd074539b1c
Fatih Mert Doğancan <fatihmertdogancan@hotmail.com>
2018-04-06 07:09:25 +0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+
+Schema::create("users",function($table){
+
+	$table->increment('id');
+	$table->float('para',3,5);
+	$table->varchar('name',30)->varchar('surname',30);
+
+	$table->last_column = true; //you should do last column before for jump to mysql syntax error (comma)
+
+	$table->varchar('email',30);
+
+	$table->compile(); //compile to sql string
+
+	echo $table; //compiled sql string
+});
